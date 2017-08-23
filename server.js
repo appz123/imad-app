00179e4989c2e67eaf1b699e1c729ffdbcdc6edf
appz123 <arpitha723@gmail.com>
2017-08-23 13:27:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool=reqquire('pg').pool;
+var config={
+    user: 'arpitha723',
+    database: 'arpitha723',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password:process.env.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -22,6 +30,17 @@ app.get('/article-two', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+var pool=new pool(config);
+app.get('/test-db',function(req,res){
+    
+    pool.query('SELECT * FROM test',function(err,result){
+        if (err){
+            res.status(500).send(err.toString());
+        }else
+        res.send(JSON.Stringify(reult));
+    })
+    
 });
 
 
