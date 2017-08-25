d1d6a76a96c46a89825edf285a9e5606ff6b4f52
appz123 <arpitha723@gmail.com>
2017-08-25 10:27:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool=reqquire('pg').pool;
+//var pool=reqquire('pg').pool;
 /*var config={
     user: 'arpitha723',
     database: 'arpitha723',
