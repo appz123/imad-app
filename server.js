9af1c2884bcfbae6a8330dda423085712e73acbc
appz123 <arpitha723@gmail.com>
2017-08-25 09:54:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get('/test-db',function(req,res){
             res.status(500).send(err.toString());
         }else
         {
-            res.send(JSON.Stringify(reult));
+            res.send(JSON.Stringlfy(reult));
         }
     });
     
