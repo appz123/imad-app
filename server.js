52c61d44015a74aa72420869b7dfc884631a8f1f
appz123 <arpitha723@gmail.com>
2017-08-25 09:52:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config={
     host: 'db.imad.hasura-app.io',
     port: '5432',
     password:process.env.DB_PASSWORD
-}
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -41,7 +41,7 @@ app.get('/test-db',function(req,res){
         {
             res.send(JSON.Stringify(reult));
         }
-    })
+    });
     
 });
 
